d07655ae237bbed7771ce3e31d0b97a37f8160f0
Puncoz Nepal <puncoz138@gmail.com>
2022-05-19 10:21:12 +0545

    data api

--- pages/api/data.ts
+++ pages/api/data.ts
@@ -1,15 +1,14 @@
 import {
     NextApiRequest,
     NextApiResponse,
-}                from "next"
-import firestore from "../../utils/firestore"
-
-const municipality = "kathmandu"
+}                 from "next"
+import { config } from "../../config/config"
+import firestore  from "../../utils/firestore"
 
 const Data = async (req: NextApiRequest, res: NextApiResponse) => {
-    const collections = await firestore.collection(municipality).get()
+    const collections = await firestore.collection(config.municipality).get()
 
-    res.send("hello world")
+    res.json(collections.docs.map(doc => doc.data()))
 }
 
 export default Data
